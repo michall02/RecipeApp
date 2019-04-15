7739165106d0a5702389c68d60649804fd35297f
Michał Kasprzak <michalxkasprzak@gmail.com>
2019-04-15 13:48:50 +0200

    refactor controllers

--- src/main/java/pl/home/recipeapp/controllers/IngredientController.java
+++ src/main/java/pl/home/recipeapp/controllers/IngredientController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import pl.home.recipeapp.services.IngredientService;
 import pl.home.recipeapp.services.RecipeService;
 
@@ -20,16 +19,14 @@ public class IngredientController {
         this.ingredientService = ingredientService;
     }
 
-    @GetMapping
-    @RequestMapping("/recipe/{recipeId}/ingredients")
+    @GetMapping("/recipe/{recipeId}/ingredients")
     public String listIngredient(@PathVariable String recipeId, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
 
         return "recipe/ingredient/list";
     }
 
-    @GetMapping
-    @RequestMapping("recipe/{recipeId}/ingredient/{id}/show")
+    @GetMapping("recipe/{recipeId}/ingredient/{id}/show")
     public String showRecipeIngredient(@PathVariable String recipeId, @PathVariable String id, Model model){
         model.addAttribute("ingredient", ingredientService.findByRecipeAndIngredientId(Long.valueOf(recipeId), Long.valueOf(id)));
         return "recipe/ingredient/show";
